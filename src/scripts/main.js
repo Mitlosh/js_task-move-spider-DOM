a21795758ed313a438e53f456c2d6f4d371774b7
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-05-26 15:05:17 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,8 +9,7 @@ document.querySelector('.wall').addEventListener('click', (e) => {
   }
 
   const wallParams = wall.getBoundingClientRect();
-  let spiderLeft =
-    e.clientX - wallParams.left - wall.clientLeft - spider.offsetWidth / 2;
+  let spiderLeft = e.clientX - wallParams.left - spider.offsetWidth / 2;
 
   if (spiderLeft < 0) {
     spiderLeft = 0;
@@ -22,8 +21,7 @@ document.querySelector('.wall').addEventListener('click', (e) => {
     spiderLeft = maxLeftPosition;
   }
 
-  let spiderTop =
-    e.clientY - wallParams.top - wall.clientTop - spider.offsetHeight / 2;
+  let spiderTop = e.clientY - wallParams.top - spider.offsetHeight / 2;
 
   if (spiderTop < 0) {
     spiderTop = 0;
